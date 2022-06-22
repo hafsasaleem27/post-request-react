a0622a35688724a26eb8292580ac6cdaf79bd715
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-06-22 16:21:10 +0500

    fixed code for fetching movies and removed extra code

--- src/App.js
+++ src/App.js
@@ -16,23 +16,25 @@ function App() {
       const response = await fetch(
         "https://react-http-fe5e1-default-rtdb.firebaseio.com/movies.json"
       );
-      console.log('response: ', response);
 
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
+      console.log('data: ', data);
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          name: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -42,20 +44,6 @@ function App() {
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
-
-  async function addMovieHandler(movie) {
-    console.log(movie);
-    const response = await fetch(
-      "https://react-http-fe5e1-default-rtdb.firebaseio.com/movies.json",
-      {
-        method: "POST",
-        body: JSON.stringify(movie),
-      }
-    );
-    const data = await response.json();
-
-    console.log("data", data);
-  }
 
   let content = <p>Found no movies.</p>;
 
@@ -74,7 +62,7 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <AddMovie onAddMovie={addMovieHandler} />
+        <AddMovie />
       </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
